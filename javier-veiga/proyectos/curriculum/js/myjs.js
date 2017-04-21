52e0c430fb7cd6676afcd4d12632cf3e1ef59b7c
ljveiga2000 <ljveiga2000@gmail.com>
2017-04-21 13:54:48 +0200

    jquery adaptacion basica

--- javier-veiga/proyectos/curriculum/js/myjs.js
+++ javier-veiga/proyectos/curriculum/js/myjs.js
@@ -1,26 +1,29 @@
-$(document).ready(function ({
+$(document).ready(function () {
 
-    $(".mostrar-datos").click(function ({
+    $("#cuadro-educacion").hide();
+    $("#cuadro-exp-laboral").hide();
+
+    $(".mostrar-datos").click(function () {
 
 
         $("#cuadro-datos-personales").show();
         $("#cuadro-educacion").hide();
         $("#cuadro-exp-laboral").hide();
-    });)
+    });
 
-    $(".mostrar-educacion").click(function ({
+    $(".mostrar-educacion").click(function () {
 
 
         $("#cuadro-datos-personales").hide();
         $("#cuadro-educacion").show();
         $("#cuadro-exp-laboral").hide();
-    });)
+    });
 
-    $(".mostrar-exp").click(function ({
+    $(".mostrar-exp").click(function () {
 
 
         $("#cuadro-datos-personales").hide();
         $("#cuadro-educacion").hide();
         $("#cuadro-exp-laboral").show();
-    });)
-}));
+    });
+});
