4b0268787d4a0c8ad4e07907d7d18d84035fc72e
ljveiga2000 <ljveiga2000@gmail.com>
2017-04-24 13:17:46 +0200

    cambios jquery dinamismo insercion educacion

--- javier-veiga/proyectos/curriculum/js/myjs.js
+++ javier-veiga/proyectos/curriculum/js/myjs.js
@@ -1,29 +1,52 @@
 $(document).ready(function () {
 
+    /*--------------------------------------------------*/
+    /*--------  transicion entre formularios   ---------*/
+    /*--------------------------------------------------*/
+
+    //ocultamos los dos cuadros secundarios
     $("#cuadro-educacion").hide();
     $("#cuadro-exp-laboral").hide();
 
-    $(".mostrar-datos").click(function () {
 
+    //dependiendo de que boton pulsemos mostraremos u ocultaremos un cuadro en concreto.
+
+    //mostramos el cuadro de datos personales
+    $(".mostrar-datos").on("click", function (event) {
 
         $("#cuadro-datos-personales").show();
         $("#cuadro-educacion").hide();
         $("#cuadro-exp-laboral").hide();
+
     });
+    //mostramos el cuadro de educacion
+    $(".mostrar-educacion").on("click", function (event) {
 
-    $(".mostrar-educacion").click(function () {
-
-
-        $("#cuadro-datos-personales").hide();
         $("#cuadro-educacion").show();
+        $("#cuadro-datos-personales").hide();
         $("#cuadro-exp-laboral").hide();
+        $(".datos-edu").hide();
+
     });
+    //mostramos el cuadro de experiencia laboral
+    $(".mostrar-exp").on("click", function (event) {
 
-    $(".mostrar-exp").click(function () {
-
-
+        $("#cuadro-exp-laboral").show();
         $("#cuadro-datos-personales").hide();
         $("#cuadro-educacion").hide();
-        $("#cuadro-exp-laboral").show();
+
+    });
+
+    /*--------------------------------------------------*/
+    /*------fin transicion entre formularios   ---------*/
+    /*--------------------------------------------------*/
+
+    $(".anadir").on("click", function (event) {
+        var desde = $("#desde-edu").val();
+        var hasta = $("#hasta-edu").val();
+        var edutextarea = $("#educacion").val();
+        $("#lista-edu").append("<li>Desde " + desde + " Hasta " + hasta + " : " + edutextarea + "</li>");
+        $("li").css("width", "80%");
+        $(".datos-edu").show();
     });
 });
