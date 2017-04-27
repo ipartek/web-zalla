ae9edb8cd74121b68ddc0a880ef0b07647af3080
raes88 <raes88@hotmail.com>
2017-04-27 22:25:00 +0200

    juego

--- raul-alvarez/Javascript/js/partida.js
+++ raul-alvarez/Javascript/js/partida.js
@@ -1,33 +1,45 @@
+'use strict';
 window.onload = alCargar;
 
 
 function alCargar() {
+     document.getElementById('btnPartida').onclick = partida;
+}
+
+
+
+function partida() {
     //var numeroMenor = prompt('Dime el numero menor');
     // var numeroMayor = prompt('Dime el numero mayor');
     var numeroAleatorio = parseInt(Math.random() * 100) + 1;
     console.log(numeroAleatorio);
-    var numeroTecleado = prompt('Introduce un numero');
+    var numeroTecleado = parseInt(prompt('Introduce un numero del 1 al 100'));
     console.log(numeroTecleado);
+    var numeroIntentos = 0;
 
-do {
-    if (numeroAleatorio > numeroTecleado) {
-        numeroTecleado = prompt('El numero es mayor');
-    }
-    if (numeroAleatorio < numeroTecleado) {
-        numeroTecleado = prompt('El numero es menor');
-    }
-    else{
-         alert('Ace(rtaste el numero , el resultado era "' + numeroAleatorio + '"');
-        break;
-    }
+    do {
+        if (isNaN(numeroTecleado)) {
+            alert('Solo numeros')
+            numeroTecleado = numeroAleatorio + 1;
+            console.log(numeroTecleado);
+        } else {
+            if (numeroTecleado >= 1 && numeroTecleado <= 100) {
 
+                if (numeroAleatorio > numeroTecleado) {
+                    numeroTecleado = parseInt(prompt('El numero es mayor,sigue intentandolo'));
+                }
+                if (numeroAleatorio < numeroTecleado) {
+                    numeroTecleado = parseInt(prompt('El numero es menor,sigue intentandolo'));
+                }
+
+            } else {
+                console.log('Solo numeros del 1 al 100')
+            }
+
+        }
+        numeroIntentos++;
+    }
+    while (numeroTecleado != numeroAleatorio);
+    alert('Acertaste el numero , el resultado era "' + numeroAleatorio + '"');
+    alert('Necesitaste "' + numeroIntentos + '" intentos');
 }
-while (numeroTecleado==numeroAleatorio);
-
-   
-
-}
-
-
-
-// alert('Hola que tal\nO\'Hara dice que tu nombre es "' + nombreIntroducido + '"');
