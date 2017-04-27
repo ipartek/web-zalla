df94a05aa73832aab047596763713e98e590adab
inigo001 <ivique001@gmail.com>
2017-04-27 10:27:30 +0200

    :dancer: js
    ini js

--- inigo-vique/proyectos/javascript/js/ejemplo-6.js
+++ inigo-vique/proyectos/javascript/js/ejemplo-6.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 function activateButtons()
 {
@@ -7,6 +7,9 @@ function activateButtons()
     document.getElementById('btn-3').onclick = f_btn3;
     document.getElementById('btn-4').onclick = f_btn4;
     document.getElementById('btn-5').onclick = f_btn5;
+    document.getElementById('btn-6').onclick = f_btn6;
+    document.getElementById('btn-7').onclick = f_btn7;
+    document.getElementById('btn-8').onclick = f_btn8;
 }
 
 function f_btn1()
@@ -77,6 +80,86 @@ function f_btn5()
         console.log(" ¡NO PASAS MENDRUGO º(`o´)º! ");
     }
 
+}
+
+function f_btn6()
+{
+    console.info("IF CON ELSE IF");
+
+    let edad = prompt("dame tu edad");
+
+    if (edad >= 18)
+    {
+        console.log("PASA");
+        console.log(" BIENVENIDO A LA DISCO ");
+    }
+    else if (edad <= 99)
+    {
+        console.log(" DEMASIADO VIEJO ");
+    }
+    else
+    {
+        console.log(" ¡NO PASAS MENDRUGO º(`o´)º! ");
+    }
+
+}
+
+function f_btn7()
+{
+
+    console.info("SWITCH");
+
+    let nivel = 3;
+
+    switch (nivel)
+    {
+        case 1:
+            console.log("Nivel 1");
+            break;
+        case 2:
+            console.log("Nivel 2");
+            break;
+        case 3:
+            console.log("Nivel 3");
+            break;
+        default:
+            console.log("NIVEL DESCONOCIDO");
+            break;
+    }
+
+    // NUNCA OLVIDAR EL BREAK
+    // Se entra por el case y se sale por el break. Se ejecuta todo hasta que encuentre
+    // un break;
+
+}
+
+function f_btn8()
+{
+    console.info("SWITCH CON GRUPOS MULTIPLES");
+
+    let mes = 9,
+        dias;
+
+    switch (mes)
+    {
+        case 2:
+            dias = 28;
+            break;
+
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            dias = 30;
+            break;
+
+        default:
+            dias = 31;
+            break;
+    }
+
+    // UML - Diagramas de flujo
+    console.log(dias);
 }
 
 window.onload = activateButtons;
