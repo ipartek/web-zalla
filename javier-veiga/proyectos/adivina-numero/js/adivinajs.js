ade32cc635e397297d727e33e41029ca8848183e
ljveiga2000 <ljveiga2000@gmail.com>
2017-04-28 09:38:04 +0200

    depuracion de codigo

--- javier-veiga/proyectos/adivina-numero/js/adivinajs.js
+++ javier-veiga/proyectos/adivina-numero/js/adivinajs.js
@@ -1,23 +1,32 @@
 function adivina() {
-    var desde = parseInt(prompt("introduce un numero desde:"));
-    var hasta = parseInt(prompt("introduce un numero hasta:"));
 
-    var numerorandom = getRandomInt(desde, hasta);
-    var intentos = 0;
     do {
+        var desde = parseInt(prompt("introduce un numero desde:"));
+    } while (isNaN(desde));
 
+    do {
+        var hasta = parseInt(prompt("introduce un numero hasta:"));
+    }
+    while (isNaN(hasta) || (hasta <= desde));
+
+    var numerorandom = parseInt(getRandomInt(desde, hasta));
+    var intentos = 0;
+
+    //do {
+    do {
         var numerousuario = parseInt(prompt("Intenta adivinar el numero : "));
-        intentos++;
+    } while (isNaN(numerousuario));
+    intentos++;
 
-        if (numerousuario > numerorandom)
-            alert("es menor");
-        else if (numerousuario < numerorandom)
-            alert("es mayor");
+    if (numerousuario > numerorandom)
+        alert("es menor");
+    else if (numerousuario < numerorandom)
+        alert("es mayor");
 
 
-    } while (numerousuario != numerorandom);
+    //} while (numerousuario != numerorandom);
 
-    
+
     alert("enhorabuena has acertado lo has hecho en " + intentos + " intentos");
 
 }
