32373bee499cc05d9edd282c7d5ae4b8c76e0253
JoelPrada <joel.pradah@gmail.com>
2017-05-04 09:18:21 +0200

    cambios

--- joel-prada/Proyecto/Pruebas/Pruebas Java/js/ejemploj5.js
+++ joel-prada/Proyecto/Pruebas/Pruebas Java/js/ejemploj5.js
@@ -4,7 +4,7 @@ var ventana = window.open(
     "http://www.domainname.ext/path/ImgFile.png",
     "DescriptiveWindowName",
     
-    "top=200, left=300 width=400, height=230, resizable, scrollbars=yes, estatus=1"
+    "top=200, left=300 width=400, height=230, resizable, scrollbars=yes, estatus=1",
 );
 
 var d = ventana.document;
