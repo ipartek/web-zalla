497275a23dbee6324a10d6a0d3daf486aa082cbc
Jon Ander Oribe <Disaicold@gmail.com>
2017-05-04 10:09:46 +0200

    upd arrays

---
+++ jon-ander-oribe/proyectos/pruebas/js/ejemplo6-script.js
@@ -0,0 +1,9 @@
+var arr = new Array();
+console.log(arr.length);
+
+var arr5 = new Array(5);
+console.log(arr5.length);
+
+var arrPrerrellenado = new Array("Hola", new Date(), 5) //Se puede pero...
+var diasMEses = new Array(31, 28, 30);
+var nombreMeses = new Array("Enero", "Febrero");
