42dc725c3e641bae716f9cdb5f271bdf3feb7c50
Jon Ander Oribe <Disaicold@gmail.com>
2017-05-04 13:38:13 +0200

    upd11

--- jon-ander-oribe/proyectos/pruebas/js/ejemplo6-script.js
+++ jon-ander-oribe/proyectos/pruebas/js/ejemplo6-script.js
@@ -5,5 +5,21 @@ var arr5 = new Array(5);
 console.log(arr5.length);
 
 var arrPrerrellenado = new Array("Hola", new Date(), 5) //Se puede pero...
-var diasMEses = new Array(31, 28, 30);
+var diasMeses = new Array(31, 28, 30);
 var nombreMeses = new Array("Enero", "Febrero");
+
+var tablero = new Array(8);
+for (int i = 0; i < 8; i++) {
+    tablero[i] = new Array(8);
+}
+console.log(tablero);
+
+var orden = [4, 2, 67, 5, 4].sort((a, b) => a - b);
+
+console.log(orden);
+
+var orden2 = [4, 2, 67, 5, 4].sort(function(a, b) {
+    return a - b;
+});
+
+console.log(orden2);
