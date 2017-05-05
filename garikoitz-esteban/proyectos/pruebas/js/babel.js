1d789535db38e9474c24fe8ae2e60d5fab6242cd
Logsly <logsly20@gmail.com>
2017-05-05 13:57:06 +0200

    nuevo ejercicio de js y copiado el de babel de Javier Lete

---
+++ garikoitz-esteban/proyectos/pruebas/js/babel.js
@@ -0,0 +1,9 @@
+"use strict"
+const getMessage = () => "Hola papasote!";
+alert(getMessage());
+
+window.onload = function () {
+    var usuario = document.getElementById('usuario')
+}
+
+console.log(usuario)
