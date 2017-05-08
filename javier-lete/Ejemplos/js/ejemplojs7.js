23565421745661bf5bc0b175f8e9fdab5dd767c4
javierlete <jglete@gmail.com>
2017-05-08 14:02:26 +0200

    Eventos blur y focus / corrección de bloque0

--- javier-lete/Ejemplos/js/ejemplojs7.js
+++ javier-lete/Ejemplos/js/ejemplojs7.js
@@ -10,18 +10,28 @@ window.onload = function() {
     txtMensajes = document.getElementById('txtMensajes');
     form = document.forms[0]; //document.getElementById("");
     
-    form.onsubmit = envioFormulario;
+    form.onsubmit = validacionDni;
     
-    var sBloque = form['sBloque'];
+    var sBloque = form.sBloque; //form['sBloque']
     sBloque.onchange = seleccionBloque;
     
     var btnPruebas = document.getElementById('btnPruebas');
     btnPruebas.onclick = pruebas;
+    
+    var txtDni = form.txtDni;
+    txtDni.onfocus = function() {
+        txtDni.value = "";
+        //txtDni.select();
+    };
+    
+    txtDni.onblur = function() {
+        validacionDni();
+    };
 };
 
-function envioFormulario() {
+function validacionDni() {
     //var txtDni = document.getElementById('txtDni');
-    var txtDni = form['txtDni']; // form['name']
+    var txtDni = form.txtDni; //form['txtDni']; // form['name']
 
     var dni = txtDni.value;
 
@@ -43,16 +53,22 @@ function pruebas() {
     txtMensajes.placeHolder = "Modificado al menos una vez";
 
     //document.getElementById('txtMensajes').value = "Hola " + //document.getElementById('txtUsuario').value;
+    
+    console.debug("DEBUG");
+    console.info("INFO");
+    console.log("LOG");
+    console.warn("WARN");
+    console.error("ERROR");
+    
+    document.forms[0].txtDni.focus();
 }
-
-
 
 function seleccionBloque() {
     var sBloque = document.getElementById("sBloque");
     
     ocultarBloques();
     
-    var bloque = "bloque"+sBloque.value;
+    var bloque = "bloque" + sBloque.value;
     console.log(bloque)
     mostrarBloque(bloque);
 }
@@ -65,11 +81,17 @@ function ocultarBloques() {
 function ocultarBloque(idBloque) {
     var bloque = document.getElementById(idBloque);
     
-    bloque.style.display = "none";    
+    if(bloque != null) {
+        bloque.style.display = "none";
+    }
 }
 
 function mostrarBloque(idBloque) {
     var bloque = document.getElementById(idBloque);
     
-    bloque.style.display = "block";
+    if(bloque != null) {
+        bloque.style.display = "block";
+    } else {
+        console.warn("No existe el bloque " + idBloque);
+    }
 }
