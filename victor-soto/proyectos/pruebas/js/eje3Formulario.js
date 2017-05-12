04e02bec1e41106e2de5459366a2502faf6bc075
Godir <godir85@gmail.com>
2017-05-12 13:57:56 +0200

    eje3 con error

--- victor-soto/proyectos/pruebas/js/eje3Formulario.js
+++ victor-soto/proyectos/pruebas/js/eje3Formulario.js
@@ -1,6 +1,9 @@
+//Inserta una línea en la siguiente posición a la input
+//document.forms[0].txtDni.parentNode.nsertBefore(document.createElement("hr"), txtDni.nextSibling)
+
 "use strict"
 
-var form, nombre, apellidos, dni, validarPatron, sexo, dia, mes, anio;
+var form, nombre, apellidos, dni, validarPatron, sexo, dia, mes, anio, texto;
 var patron = /^([A-Z]{1}[a-zñáéíóú]+[\s]*)+$/;
 var patronDni = /^\d{8}[a-zA-Z]$/;
 
@@ -25,7 +28,7 @@ function validar() {
         }
         if (!evaluarApellidos()) {
             apellidos.focus();
-            return false;    
+            return false;
         }
         if (!evaluarDni()) {
             dni.focus();
@@ -35,15 +38,15 @@ function validar() {
             sexo.focus();
             return false;
         }
-        if(!evaluarDia()){
+        if (!evaluarDia()) {
             dia.focus();
             return false;
         }
-        if(!evaluarMes()){
+        if (!evaluarMes()) {
             mes.focus();
             return false;
         }
-        if(!evaluarAnio()){
+        if (!evaluarAnio()) {
             anio.focus();
             return false;
         }
@@ -58,7 +61,8 @@ function evaluarNombre() {
     var nombreValor = nombre.value;
     validarPatron = patron.test(nombreValor);
     if (!validarPatron) { // || !correcto(compNombre)) {
-        alert("¡¡¡NO has introducido correctamente el nombre!!!\nEjemplo: Luis Angel");
+        texto = "¡¡¡NO has introducido correctamente el nombre!!!\nEjemplo: Luis Angel";
+        error(nombre, texto);
         return false;
     } else {
         return true;
@@ -69,7 +73,8 @@ function evaluarApellidos() {
     var apellidosValor = apellidos.value;
     validarPatron = patron.test(apellidosValor);
     if (!validarPatron) { // || !correcto(compNombre)) {
-        alert("¡¡¡NO has introducido correctamente los apellidos!!!\nEjemplo: Marquez Robles");
+        texto = "¡¡¡NO has introducido correctamente los apellidos!!!\nEjemplo: Marquez Robles";
+        error(apellidos, texto);
         return false;
     } else {
         return true;
@@ -79,7 +84,8 @@ function evaluarApellidos() {
 function evaluarDni() {
     var dniValor = dni.value;
     if (!validarDni(dniValor)) {
-        alert("¡¡¡NO has introducido correctamente el dni!!!\nEjemplo: 12345678Z");
+        texto = "¡¡¡NO has introducido correctamente el dni!!!\nEjemplo: 12345678Z";
+        error(dni, texto);
         return false;
     } else {
         return true;
@@ -88,34 +94,38 @@ function evaluarDni() {
 
 function evaluarSexo() {
     if (sexo.value == 0) {
-        alert("¡¡¡NO has seleccionado sexo!!!");
+        texto = "¡¡¡NO has seleccionado sexo!!!";
+        error(sexo, texto);
         return false;
     } else {
         return true;
     }
 }
 
-function evaluarDia(){
-    if(dia.value == 0){
-        alert("¡¡¡NO has seleccionado un día!!!");
+function evaluarDia() {
+    if (dia.value == 0) {
+        texto = "¡¡¡NO has seleccionado un día!!!";
+        error(dia, texto);
         return false;
     } else {
         return true;
     }
 }
 
-function evaluarMes(){
-    if(mes.value == 0){
-        alert("¡¡¡NO has seleccionado un mes!!!");
+function evaluarMes() {
+    if (mes.value == 0) {
+        texto = "¡¡¡NO has seleccionado un mes!!!";
+        error(mes, texto);
         return false;
     } else {
         return true;
     }
 }
 
-function evaluarAnio(){
-    if(anio.value == 0){
-        alert("¡¡¡NO has seleccionado un año!!!");
+function evaluarAnio() {
+    if (anio.value == 0) {
+        texto = "¡¡¡NO has seleccionado un año!!!";
+        error(anio, texto);
         return false;
     } else {
         return true;
@@ -126,7 +136,63 @@ function evaluarIdiomas() {
     if (form.idioma1.checked || form.idioma2.checked || form.idioma3.checked) {
         return true;
     } else {
-        alert("¡¡¡NO has seleccionado ningún idioma!!!");
+        texto = "¡¡¡NO has seleccionado ningún idioma!!!";
+        error(form.idioma1, texto);
         return false;
     }
+}
+
+function error(elemento, texto) {
+    var altoDisplay = window.innerHeight;
+    var anchoDisplay = window.innerWidth;
+
+    var divFondo = document.createElement("div");
+
+    divFondo.id = "popupfondo";
+
+    divFondo.style.backgroundColor = "rgba(50, 50, 50, 0.8)";
+    divFondo.style.position = "fixed";
+    divFondo.style.top = 0;
+    divFondo.style.left = 0;
+    divFondo.style.height = altoDisplay + "px";
+    divFondo.style.width = anchoDisplay + "px";
+    divFondo.style.zIndex = 10000;
+
+    document.body.appendChild(divFondo);
+
+    var x, y, alto, ancho;
+
+    alto = altoDisplay / 2;
+    ancho = anchoDisplay / 2;
+
+    x = ancho / 2;
+    y = alto / 2;
+
+    var divMensaje = document.createElement("div");
+
+    divMensaje.id = "popcuadromensaje";
+
+    divMensaje.style.backgroundColor = "white";
+    divMensaje.style.position = "fixed";
+    divMensaje.style.top = y + "px";
+    divMensaje.style.left = x + "px";
+    divMensaje.style.height = alto + "px";
+    divMensaje.style.width = ancho + "px";
+    divMensaje.style.zIndex = 10001;
+    divMensaje.style.textAlign = "center";
+    divMensaje.style.fontSize = "2em";
+
+    divFondo.appendChild(divMensaje);
+
+    divMensaje.innerHTML = "<h1>ERROR</h1><p>"+ texto +"</p>";
+
+    var boton = document.createElement("button");
+
+    boton.innerHTML = "ACEPTAR";
+    boton.onclick = function () {
+        divFondo.parentNode.removeChild(divFondo);
+        elemento.focus();
+    };
+
+    divMensaje.appendChild(boton);
 }
