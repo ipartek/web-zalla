6eaafbce1ffca06b4cae0dbd7ad4b2a9f838d4b0
Galder30 <galderenrique@hotmail.com>
2017-05-12 10:32:37 +0200

    Últimos updates - validación y popup

--- galder-enrique/proyectos/ejercicios-js/ejercicio-js3/js/ejercicios-js3.js
+++ galder-enrique/proyectos/ejercicios-js/ejercicio-js3/js/ejercicios-js3.js
@@ -1,21 +1,91 @@
 'use strict'
+//DEFINICION DE VARIABLES
 var form, txtNombre = "",
-    txtApellidos, dni, chkIdioma, edad, validarDia, validarMes, validarAno, salida = false,
+    txtApellidos, txtDni, chkIdioma, edad, validarDia, validarMes, validarAno, salida = false,
     validarCastellano, validarIngles, validarFrances,
     bloqueMensajes, valorSexo, valorProfesion = "",
     valorTasas;
 
+//EVENTO ONLOAD
 window.onload = function() {
     form = document.forms[0];
     form.txtNombre.focus();
 
+    txtDni = document.getElementById("txtDni");
+    txtDni.onblur = validarFormDni;
+
+
+    //EVENTO ONSUBMIT
     form.onsubmit = function() {
+
         if (!validarFecha() || !validarIdioma()) {
             return false;
-        } else {
-            alert("OEEEEEEEEEEEEEE, HAS HECHO EL FORMULARIO CORRECTAMENTE!!!!!!!111UNOUNOUNO");
+        } else
+        // {
+        //     alert("OEEEEEEEEEEEEEE, HAS HECHO EL FORMULARIO CORRECTAMENTE!!!!!!!111UNOUNOUNO");
+        // }
+
+        //VALIDACION PARA SACAR POPUP EN CASO DE TRUE
+        if (validarFormDni()) {
+            var altoDisplay = window.innerHeight;
+            var anchoDisplay = window.innerWidth;
+
+            var divFondo = document.createElement("div");
+
+            divFondo.id = "popupfondo";
+
+            divFondo.style.backgroundColor = "rgba(50, 50, 50, 0.8)";
+            divFondo.style.position = "fixed";
+            divFondo.style.top = 0;
+            divFondo.style.left = 0;
+            divFondo.style.height = altoDisplay + "px";
+            divFondo.style.width = anchoDisplay + "px";
+            divFondo.style.zIndex = 10000;
+
+            document.body.appendChild(divFondo);
+
+            var x, y, alto, ancho;
+
+            alto = altoDisplay / 2;
+            ancho = anchoDisplay / 2;
+
+            x = ancho / 2;
+            y = alto / 2;
+
+            var divMensaje = document.createElement("div");
+
+            divMensaje.id = "popcuadromensaje";
+
+            divMensaje.style.backgroundColor = "white";
+            divMensaje.style.position = "fixed";
+            divMensaje.style.top = y + "px";
+            divMensaje.style.left = x + "px";
+            divMensaje.style.height = alto + "px";
+            divMensaje.style.width = ancho + "px";
+            divMensaje.style.zIndex = 10001;
+            divMensaje.style.textAlign = "center";
+            divMensaje.style.fontSize = "2em";
+
+            divFondo.appendChild(divMensaje);
+
+            divMensaje.innerHTML = "<h1>OJO</h1><p>¡SE VA A ENVIAR EL FORMULARIO!</p>";
+
+            var boton = document.createElement("button");
+
+            boton.innerHTML = "ACEPTAR";
+            boton.onclick = function() {
+                divFondo.parentNode.removeChild(divFondo);
+                document.forms[0].submit();
+            };
+
+            divMensaje.appendChild(boton);
         }
+        return false;
+
+
     };
+
+
 
     edad = document.getElementById("txtEdad");
     edad.onchange = soloNumero;
@@ -28,7 +98,7 @@ window.onload = function() {
 //document.getElementById("rProfesion").onchange = ponerTasas;
 
 
-
+//VALIDACION FECHA CORRECTA
 function validarFecha() {
     validarDia = document.getElementById("dia").value;
     validarMes = document.getElementById("mes").value;
@@ -42,6 +112,7 @@ function validarFecha() {
     }
 }
 
+//VALIDACION SELECCION DE IDIOMA
 function validarIdioma() {
     validarCastellano = document.getElementById("rCastellano");
     validarIngles = document.getElementById("rIngles");
@@ -55,6 +126,7 @@ function validarIdioma() {
     }
 }
 
+//COMPROBACION DE VALIDEZ DE EDAD 
 function soloNumero() {
     edad = document.getElementById("txtEdad");
     if (edad.value <= 1 || isNaN(edad.value)) {
@@ -62,6 +134,7 @@ function soloNumero() {
     }
 }
 
+//DATOS MOSTRADOS DESDE EL BOTON MOSTRAR
 function mostrarDatos() {
     txtNombre = document.getElementById("txtNombre").value;
     txtApellidos = document.getElementById("txtApellidos").value;
@@ -73,10 +146,10 @@ function mostrarDatos() {
         chkIdioma = document.getElementById("rCastellano").value;
     }
     if (rIngles.checked) {
-        chkIdioma += " , " + document.getElementById("rIngles").value;
+        chkIdioma += " " + document.getElementById("rIngles").value;
     }
     if (rFrances.checked) {
-        chkIdioma += " , " + document.getElementById("rFrances").value;
+        chkIdioma += " " + document.getElementById("rFrances").value;
     }
     alert("NOMBRE: " + txtNombre +
         "\nAPELLIDOS: " + txtApellidos +
@@ -92,6 +165,7 @@ function mostrarDatos() {
     //valorTasas = document.getElementById("comTasas").value;
 
 
+    //ACTIVACION DEL ESTADO CIVIL CUANDO SEXO = FEMENINO
     // function activarEstadoCivil() {
     //     valorSexo = formulario1.sSexo
     //         .options[formulario1.sSexo.selectedIndex].value;
@@ -102,6 +176,7 @@ function mostrarDatos() {
     //     }
     // }
 
+    //ASIGNACION DE TASAS DEPENDIENDO DE PROFESION
     // function ponerTasas() {
     //     valorProfesion = form.rProfesion.value;
     //     switch (valorProfesion) {
