2cc67c04b5f6e611934613316160db055da55e4a
toni1970 <garciacid1980@gmail.com>
2017-05-22 13:59:31 +0200

    eclipse
    eclipse

--- toni-garcia/JAVA/HolaMundo.bat
+++ toni-garcia/JAVA/HolaMundo.bat
@@ -1,6 +1,6 @@
 cls
 set JAVAPATH=%JAVAPATH%;C:\Program Files\Java\jdk1.8.0_131\bin
 set PATH=%PATH%;JAVAPATH%
-set CLASSPATH=%CLASSPATH%;C:\DESARROLLO\JAVA
+set CLASSPATH=%CLASSPATH%;C:\DESARROLLO\WEB\web-zalla\toni-garcia\JAVA
 JAVAC hola\HolaMundo.java
 JAVA hola.HolaMundo
