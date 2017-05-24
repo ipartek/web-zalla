85781ea15bb1873fa78bcce9aac2166edd41546d
GorkaPA <gorkitap@hotmail.com>
2017-05-24 10:37:35 +0200

    ...

--- gorka-perez/proyectos/java/hola/HolaMundo.java
+++
@@ -1,9 +0,0 @@
-package hola;
-
-public class HolaMundo
-{
-	public static void main(String[] args)
-	{
-		System.out.println("hola mundo");
-	}
-}
--- gorka-perez/proyectos/java/holaMundo.bat
+++
@@ -1,6 +0,0 @@
-cls
-set JAVAPATH=%JAVAPATH%;C:\Program Files\Java\jdk1.8.0_131\bin
-set PATH=%PATH%;%JAVAPATH%
-set CLASSPATH=%CLASSPATH%;C:\desarrollo\web\web-zalla\gorka-perez\proyectos\java
-JAVAC hola\HolaMundo.java
-JAVA hola.HolaMundo
