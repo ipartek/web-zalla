621570f24587798ddbd6e95c528a20b5fa98d32f
JoelPrada <joel.pradah@gmail.com>
2017-05-24 10:11:46 +0200

    JAva

---
+++ joel-prada/Proyecto/Pruebas/Pruebas Java/EjecutarHolaMundo.bat
@@ -0,0 +1,6 @@
+cls
+set JAVAPATH=%"JAVAPATH"%; C:\Program Files\Java\jdk1.8.0_121\bin
+set PATH=%PATH%;%JAVAPATH%
+set CLASSPATH=%CLASSPATH%;C:\desarrollo\web\web-zalla\joel-prada\Proyecto\Pruebas\Pruebas Java
+JAVAC hola\HolaMundo.java
+JAVA hola.HolaMundo
