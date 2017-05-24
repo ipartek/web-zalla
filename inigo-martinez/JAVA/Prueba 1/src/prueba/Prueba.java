521b68f2f7f7de2b2d353f892c9cb1f7739194d5
Jonan <Disaicold@gmail.com>
2017-05-24 20:35:11 +0200

    Raza añadida para testear el proceso de creación y algunos cambios en el menu del main

--- inigo-martinez/JAVA/Prueba 1/src/prueba/Prueba.java
+++ inigo-martinez/JAVA/Prueba 1/src/prueba/Prueba.java
@@ -8,6 +8,7 @@ public class Prueba {
 
 	static Humanos Humanos;
 	static Orcos Orcos;
+	static Goblins Goblins;
 
 	public static void main(String args[]) {
 
@@ -18,9 +19,11 @@ public class Prueba {
 		System.out.println("Elige uno de los bandos:");
 		System.out.println("1.- Humanos");
 		System.out.println("2.- Orcos");
+		System.out.println("3.- Los poderosos Goblins[Aun en fase Alpha]");
 		System.out.println("Recuerdo que las aldeas se defienden con un minimo:");
 		System.out.println("Humanos ---> 20 soldados");
 		System.out.println("Orcos ---> 50 soldados");
+		System.out.println("Goblins ---> 80 soldados");
 
 		eleccion = teclado.nextInt();
 
@@ -39,16 +42,35 @@ public class Prueba {
 			break;
 
 		case 2:
-
+			System.out.print("Introduce la cantidad de soldados de tu ejercito: ");
 			tamanio = teclado.nextInt();
 			// ORCOS
 			Orcos = new Orcos(tamanio);
 			// HUMANOS
 			if (tamanio > 120) {
-				Humanos = new Humanos(tamanio /** - 100 **/);
+				Humanos = new Humanos(tamanio /** - 100 **/
+				);
 			} else {
 				Humanos = new Humanos(20);
 			}
+			break;
+
+		case 3:
+			System.out.println("Introduce la cantidad de soldados de tu ejercito: ");
+			tamanio = teclado.nextInt();
+			// GOBLINS
+			Goblins = new Goblins(tamanio);
+			// HUMANOS
+			if (tamanio > 120) {
+				Humanos = new Humanos(tamanio /** - 100 **/
+				);
+			} else {
+				Humanos = new Humanos(20);
+			}
+
+		default:
+			System.out.println("Opción no valida");
+			
 
 		}
 
