491e3423b26ba2b186c7f4f96474b95c0f64eb5b
raes88 <raes88@hotmail.com>
2017-05-24 10:11:24 +0200

    examen

---
+++ raul-alvarez/Raul_java/hola/hola_mundo.java
@@ -0,0 +1,8 @@
+package hola;
+public class hola_mundo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hola Mundo!!!");
+	}
+}
--- raul-alvarez/examen curso/js/validar.js
+++ raul-alvarez/examen curso/js/validar.js
@@ -6,8 +6,8 @@ function validar() {
     var txtErrorP = document.getElementById("precio");
     var txtErrorS = document.getElementById("stock");
     var txtErrorF = document.getElementById("fecha");
-    var muestraStock=1;
-    var muestraPrecio=1;
+    var muestraStock = 1;
+    var muestraPrecio = 0;
     var toRemove;
 
     //-------------------------NOMBRE
@@ -63,10 +63,42 @@ function validar() {
         //delete txtError.error;
 
     }
+    //--------------------------precio
+
+
+    if ((formulario.precio.value <= 1) || (isNaN(formulario.precio.value))) {
+        toRemove = document.getElementById('errorPrecio');
+        if (toRemove) {
+            toRemove.remove();
+        }
+        txtErrorS = document.getElementById("precio");
+        txtErrorS.error = span = document.createElement("span");
+        span.className = "error";
+        span.setAttribute("id", "errorPrecio");
+        span.innerHTML = " Dato erroneo";
+        txtErrorS.parentNode.insertBefore(span, txtErrorS.nextSibling);
+
+        muestraPrecio = 1;
+
+        return false;
+
+    } else if (parseInt(formulario.stock.value) > 1 && muestraPrecio == 1) {
+
+        //txtError = document.getElementById("stock");
+        toRemove = document.getElementById('errorPrecio');
+        toRemove.remove();
+
+        muestraPrecio = 0;
+
+
+        //txtError.error.parentNode.removeChild(txtError.error);
+        //delete txtError.error;
+    }
+
     //--------------------------STOCK
 
-    
-    if ((parseInt(formulario.stock.value, 10) < 1) || (isNaN(formulario.stock.value))) {
+
+    if ((formulario.stock.value <= 1) || (isNaN(formulario.stock.value))) {
         toRemove = document.getElementById('errorStock');
         if (toRemove) {
             toRemove.remove();
@@ -77,65 +109,33 @@ function validar() {
         span.setAttribute("id", "errorStock");
         span.innerHTML = " Dato erroneo";
         txtErrorS.parentNode.insertBefore(span, txtErrorS.nextSibling);
-      
+
         muestraStock = 1;
-        
+
         return false;
 
     } else if (parseInt(formulario.stock.value) > 1 && muestraStock == 1) {
-        
+
         //txtError = document.getElementById("stock");
         toRemove = document.getElementById('errorStock');
-        if(toRemove){
-        toRemove.remove();
-        }
-        muestraStock = 0;
-       
-        
-        //txtError.error.parentNode.removeChild(txtError.error);
-        //delete txtError.error;
-    }
-
-     //--------------------------precio
-
-    
-    if ((parseFloat(formulario.precio.value, 10) < 1) || (isNaN(formulario.stock.value))) {
-        toRemove = document.getElementById('errorPrecio');
         if (toRemove) {
             toRemove.remove();
         }
-        txtErrorP = document.getElementById("precio");
-        txtErrorP.error = span = document.createElement("precio");
-        span.className = "error";
-        span.setAttribute("id", "errorPrecio");
-        span.innerHTML = " Dato erroneo";
-        txtErrorS.parentNode.insertBefore(span, txtErrorS.nextSibling);
-      
-        muestraPrecio = 1;
-        
-        return false;
+        muestraStock = 0;
 
-    } else if (parseInt(formulario.precio.value) > 1 && muestraPrecio == 1) {
-        
-        //txtError = document.getElementById("stock");
-        toRemove = document.getElementById('errorPrecio');
-        if(toRemove){
-        toRemove.remove();
-        }
-        muestraPrecio = 0;
-       
-        
+
         //txtError.error.parentNode.removeChild(txtError.error);
         //delete txtError.error;
     }
-    
+
+
     //-------------------------fecha
-    
+
 
     if ((formulario.fecha.value == '')) {
         toRemove = document.getElementById('errorFecha');
         if (toRemove) {
-    
+
             toRemove.remove();
         }
         txtErrorF = document.getElementById("fecha");
@@ -150,7 +150,10 @@ function validar() {
         //txtError = document.getElementById("fecha");
         //txtError.error.parentNode.removeChild(txtError.error);
         toRemove = document.getElementById('errorFecha');
-        toRemove.remove();
+        if (toRemove) {
+
+            toRemove.remove();
+        }
         counter--;
         //delete txtError.error;
     }
@@ -168,22 +171,25 @@ function validar() {
 function salir() {
 
 
-        alert("Formulario correcto !!");
-        //location.reload();
+    alert("Formulario correcto !!");
+    //location.reload();
 
-        if (confirm("Seguro Que Quieres añadirlo?")) {
-
-            alert("Registro añadido.");
-            $("#cajaFormulario").css("display", "none");
-            $(".btnOcultar").css("display", "none");
+    if (confirm("Seguro Que Quieres añadirlo?")) {
+        $("#btnAnadir").css("display", "inline");
 
 
 
-        } else {
-            $("#cajaFormulario").css("display", "none");
-            $(".btnOcultar").css("display", "none");
+        alert("Registro añadido.");
+        $("#cajaFormulario").css("display", "none");
+        $(".btnOcultar").css("display", "none");
 
-        }
+        $("#popupfondo").remove();
+
+    } else {
+        $("#cajaFormulario").css("display", "none");
+        $(".btnOcultar").css("display", "none");
+
+    }
 
 
 }
