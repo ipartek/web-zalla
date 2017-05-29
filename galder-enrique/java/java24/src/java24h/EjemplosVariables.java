9a783fedf49477a1f91d2d273699e90c63140157
Galder30 <galderenrique@hotmail.com>
2017-05-29 13:07:44 +0200

    update

---
+++ galder-enrique/java/java24/src/java24h/EjemplosVariables.java
@@ -0,0 +1,38 @@
+package java24h;
+
+public class EjemplosVariables {
+
+	public static void main(String[] args) {
+		// PRIMITIVOS
+		// Enteros
+		short a=3;
+		int b=5;
+		long c=8;
+		
+		// Booleanos
+		boolean correcto = true;
+		
+		// Caracteres
+		char letra = 'R';
+		
+		// Byte
+		byte d=12;
+		
+		// Reales
+		float e=0.12f;
+		double f=0.5734;
+		
+		/*
+		 *  Comentario de bloque
+		 */
+		// REFERENCIA
+		String cadena = "Esto es una persona";
+		Persona persona = new Persona();
+		
+		int x = 3;
+		int y = 7;
+		int z = x * y;
+
+	}
+
+}
