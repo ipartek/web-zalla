596f5bfde0047f3b9589c1feceb3e694f4714785
toni1970 <garciacid1980@gmail.com>
2017-06-01 09:28:28 +0200

    010617
    010617

--- toni-garcia/JAVA/geometria/src/geometria/Direccion.java
+++ toni-garcia/JAVA/geometria/src/geometria/Direccion.java
@@ -1,29 +1,17 @@
 package geometria;
 
+public class Direccion {
+	private int arriba;
+	private int abajo;
+	private int derecha;
+	private int izquierda;
 
-	
-	
-	public class Direccion {
-		private int ARRIBA;
-		private int ABAJO;
-		private int DERECHA;
-		private int IZQUIERDA;
-		
-		
-		public Direccion(int ARRIBA,int ABAJO,int DERECHA,int IZQUIERDA) {
-			this.ARRIBA = ARRIBA;
-			this.ABAJO = ABAJO;
-			this.DERECHA = DERECHA;
-			this.IZQUIERDA = IZQUIERDA;
-		}
-		
-			
-	
-	
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Direccion(int arriba, int abajo, int derecha, int izquierda)
+	{
+		this.arriba = arriba;
+		this.abajo = abajo;
+		this.derecha = derecha;
+		this.izquierda = izquierda;
 	}
 
 }
