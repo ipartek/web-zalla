8ca80b61bf0fe4ccdfdc7a7f90cbed1f6c4c3aaa
raes88 <raes88@hotmail.com>
2017-06-26 09:15:31 +0200

    corregido

---
+++ javier-lete/Java/AlmacenamientoPersonas-20170621T085317Z-001/AlmacenamientoPersonas/src/entradasalida/AlmacenamientoFicheros.java
@@ -0,0 +1,16 @@
+package entradasalida;
+
+import java.util.List;
+
+import beans.Persona;
+
+public class AlmacenamientoFicheros {
+
+	public static void guardarPersonas(String nombreFichero, List<Persona> listaPersonas) {
+	}
+
+	public static List<Persona> obtenerPersonas(String nombreFichero) {
+		return null;
+	}
+
+}
