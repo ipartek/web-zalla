bf80c9b29ea1942479bf000669538878d86b843e
Jon Ander Oribe <Disaicold@gmail.com>
2017-07-10 13:08:25 +0200

    upd2

--- jon-ander-oribe/Bases de Datos(MySQL)/consultasVideoteca.sql
+++ jon-ander-oribe/Bases de Datos(MySQL)/consultasVideoteca.sql
@@ -1,8 +1,10 @@
 USE videoteca;
 
 
-SELECT titulo  ,CONCAT(nombre,' ',apellido) Interprete
+SELECT titulo ,CONCAT(a.nombre,' ',a.apellido) Interprete,CONCAT(d.nombre,' ',d.apellido) Director
 FROM pelicula p 
 JOIN actores_por_pelicula app ON p.id_Pelicula=app.id_PeliculaInter 
-JOIN actor a ON app.id_ActorInter=a.id_Actor ;
+JOIN actor a ON app.id_ActorInter=a.id_Actor
+JOIN directores_por_pelicula dp ON p.id_Pelicula=dp.id_PeliculaInter
+JOIN director d ON dp.id_DirectorInter=d.id_Director ;
 
